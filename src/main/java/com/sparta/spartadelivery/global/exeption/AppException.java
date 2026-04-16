332c6e5493efa688ec391ebed40105762ae79c68
kimyubi <ieieie0419@gmail.com>
2026-04-16 17:20:08 +0900

    feat: add global exception handling

---
+++ src/main/java/com/sparta/spartadelivery/global/exeption/AppException.java
@@ -0,0 +1,20 @@
+package com.sparta.spartadelivery.global.exeption;
+
+import lombok.Getter;
+
+// 서비스/인증/도메인 로직에서 의도적으로 던지는 Exception을 ErrorCode와 함께 표현하는 클래스
+@Getter
+public class AppException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public AppException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public AppException(ErrorCode errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+}
