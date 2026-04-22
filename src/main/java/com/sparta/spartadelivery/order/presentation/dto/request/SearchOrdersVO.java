9748ab9b7900141c50aef5d221e7e20035cc139f
hyesuhan <heasoo88@gmail.com>
2026-04-22 13:03:25 +0900

    fix: SearchOrderVO class to record type

--- src/main/java/com/sparta/spartadelivery/order/presentation/dto/request/SearchOrdersVO.java
+++ src/main/java/com/sparta/spartadelivery/order/presentation/dto/request/SearchOrdersVO.java
@@ -2,19 +2,15 @@ package com.sparta.spartadelivery.order.presentation.dto.request;
 
 import com.sparta.spartadelivery.order.domain.entity.OrderStatus;
 import com.sparta.spartadelivery.user.domain.entity.Role;
-import lombok.Builder;
-import lombok.Getter;
 
 import java.util.UUID;
 
-@Getter
-@Builder
-public class SearchOrdersVO {
-    private final Long requesterId;
 
-    private final Role requesterRole;
+public record SearchOrdersVO (
+        Long requesterId,
+        Role requesterRole,
+        UUID storeId,
+        OrderStatus status
+) {
 
-    private final UUID storeId;
-
-    private final OrderStatus status;
 }
