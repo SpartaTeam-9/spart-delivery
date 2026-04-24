5afff653afed3e410a6e91645610bc73e892e7a7
kimyubi <ieieie0419@gmail.com>
2026-04-24 15:38:48 +0900

    feat: 가게 목록 조회 DTO 추가

--- src/main/java/com/sparta/spartadelivery/store/domain/repository/StoreRepository.java
+++ src/main/java/com/sparta/spartadelivery/store/domain/repository/StoreRepository.java
@@ -2,7 +2,11 @@ package com.sparta.spartadelivery.store.domain.repository;
 
 import com.sparta.spartadelivery.store.domain.entity.Store;
 import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StoreRepository extends JpaRepository<Store, UUID> {
+
+    Page<Store> findAllByDeletedAtIsNull(Pageable pageable);
 }
